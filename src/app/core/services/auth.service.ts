0d5578de660984e1751beae4bf67241834b2b212
chheikhlo <chheikhlo@gmail.com>
2023-01-03 03:36:54 +0100

    Guard & self-connection through localstorage

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -3,7 +3,7 @@ import { BehaviorSubject, Observable, of } from 'rxjs';
 import { User } from 'src/app/shared/models/user';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
-import { switchMap, tap, catchError, finalize } from 'rxjs/operators';
+import { switchMap, tap, catchError, finalize, delay } from 'rxjs/operators';
 import { UsersService } from './users.service';
 import { ErrorService } from 'src/app/core/services/error.service';
 import { LoaderService } from './loader.service';
@@ -45,9 +45,11 @@ export class AuthService {
        const userId: string = data.localId;
        const jwt: string = data.idToken;
        
+       this.saveAuthData(userId, jwt);
        return this.usersService.get(userId, jwt);
       }),
       tap(user => this.user.next(user)),
+      tap(_ => this.logoutTimer(3600)), // On déclenche la minuterie !
      // catchError(error => this.errorService.handleError(error)),
       finalize(() => this.loaderService.setLoading(false))
      );
@@ -81,16 +83,42 @@ export class AuthService {
         name: name
        });
     
+       this.saveAuthData(data.localId, jwt);
        return this.usersService.save(user, jwt);
       }),
       tap(user => this.user.next(user)),
+      tap(_ => this.logoutTimer(3600)), // On déclenche la minuterie !
       //catchError(error => this.errorService.handleError(error)),
       finalize(() => this.loaderService.setLoading(false))
      );
   }
 
   logout(): void {
+    localStorage.removeItem('expirationDate'); // Ajoutez cette ligne, 
+    localStorage.removeItem('token'); // Et celle-ci aussi,
+    localStorage.removeItem('userId'); // Et enfin celle-là !
     this.user.next(null);
     this.router.navigate(['/login']);
+  }
+
+    //On ajoute la méthode qui déclenche cette fameuse minuterie : 
+  private logoutTimer(expirationTime: number): void {
+    of(true).pipe(
+    delay(expirationTime * 1000)
+    ).subscribe(_ => this.logout());
+  }
+
+  // Pour automatiser la connexion du user une fois revenu sur l'app grace au localStorage
+  private saveAuthData(userId: string, token: string) {
+    const now = new Date();
+    const expirationDate = (now.getTime() + 3600 * 1000).toString();
+    localStorage.setItem('expirationDate', expirationDate);
+    localStorage.setItem('token', token);
+    localStorage.setItem('userId', userId);
+   }
+
+   autoLogin(user: User) {
+    this.user.next(user);
+    this.router.navigate(['app/dashboard']);
    }
 }
--- src/app/protected/protected-routing.module.ts
+++ src/app/protected/protected-routing.module.ts
@@ -7,11 +7,14 @@ import { PlanningComponent } from './planning/planning/planning.component';
 import { ProfilComponent } from './profil/profil/profil.component';
 import { WorkdayComponent } from './workday/workday/workday.component';
 import { ProtectedComponent } from './protected.component';
+import { AuthGuard } from '../core/guards/auth.guard';
 
 const routes: Routes = [
   {
   path: 'app',
   component: ProtectedComponent, 
+  canActivate: [AuthGuard],
+  canActivateChild: [AuthGuard],
   children: [
    { path: 'dashboard', component: DashboardComponent },
    { path: 'parameters', component: ParametersComponent },
